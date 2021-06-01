de847e21ce7cd7350bf0ca16b548944c1e57e703
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-06-01 12:25:27 -0700

    printString function

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-// Code your solution here!
+function printString(myString) {
+    console.log(myString[0]);
+
+    if (myString.length > 1) {
+        let mySubString = myString.substring(1, myString.length);
+        printString(mySubString);
+    } else  {
+        return true;
+    }
+}
+
+function reverseString(myString) {
+    
+}
+
+function isPalindrome(params) {
+    
+}
+
+function addUpTo(params) {
+    
+}
+
+function maxOf(params) {
+    
+}
+
+function includesNumber(params) {
+    
+}
+
+
 
